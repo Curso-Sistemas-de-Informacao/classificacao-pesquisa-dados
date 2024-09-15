e85d63032cbfda28f1e023acea128c1d34330a2e
Pedro Leal Lovatto <pedrolovatto99@gmail.com>
2024-09-15 19:05:24 -0300

    quase finalizado

--- Trabalho G1/ps_store/ps_store/spiders/psStore.py
+++ Trabalho G1/ps_store/ps_store/spiders/psStore.py
@@ -7,9 +7,30 @@ class PsstoreSpider(scrapy.Spider):
 
     def parse(self, response):
         jogos = response.css('span.psw-t-body::text').getall() 
-        precos = response.css('span.psw-m-r-3::text').getall() 
-        for jogos, precos in zip(jogos, precos):
-                yield{
-                'nome_jogo': jogos,
-                'preco_jogo': precos,
-                }
+        precos = response.css('span.psw-m-r-3::text').getall()
+
+        jogos_precos = list(zip(jogos, precos))
+
+        jogos_precos = self.bubble_sort(jogos_precos)
+
+        for nome_jogo, preco_jogo in jogos_precos:
+            yield {
+                'nome_jogo': nome_jogo,
+                'preco_jogo': preco_jogo,
+            }
+
+    def bubble_sort(self, jogos_precos):
+        tamanho = len(jogos_precos)
+
+        jogos_precos_formatados = []
+
+        for nome, preco in jogos_precos:
+            preco_formatado = float(preco.replace('R$', '').replace(',', '.').strip())
+            jogos_precos_formatados.append((nome, preco_formatado)) 
+
+        for i in range(tamanho):
+            for j in range(0, tamanho-i-1):
+                if jogos_precos_formatados[j][1] > jogos_precos_formatados[j+1][1]:
+                    jogos_precos_formatados[j], jogos_precos_formatados[j+1] = jogos_precos_formatados[j+1], jogos_precos_formatados[j]
+
+        return jogos_precos_formatados
