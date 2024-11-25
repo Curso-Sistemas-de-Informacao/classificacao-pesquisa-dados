e13e12f487543fb1b3465b687b5b8fd064efe1ea
Pedro Leal Lovatto <pedrolovatto99@gmail.com>
2024-11-25 18:45:09 -0300

    ultimo commit (espero)

--- Trabalho G2/trabalho-g2.py
+++ Trabalho G2/trabalho-g2.py
@@ -31,20 +31,25 @@ class ArvoreJogos:
 
 
     def buscar_por_preco(self, preco):
-        atual = self.raiz
-        while atual:
-            if atual.preco == preco:
-                return atual
-            elif atual.preco < preco:
-                atual = atual.esquerda
-            else:
-                atual = atual.direita
-        return None
+
+        def encontrar_jogos_preco(node, preco, jogos):
+            if node == None: 
+                return
+
+            if node.preco == preco:
+                jogos.append(node)
+
+            encontrar_jogos_preco(node.esquerda, preco, jogos)
+            encontrar_jogos_preco(node.direita, preco, jogos)
+
+        jogos_achados = []
+        encontrar_jogos_preco(self.raiz, preco, jogos_achados)
+        return jogos_achados
 
     def busca_por_faixa_preco(self, preco_minimo, preco_maximo):
 
         def achar_jogos_faixa(node, jogos):
-            if node == None:
+            if node == None: # tive que colocar isso pq chegava em um ponto que o valor era nulo, aí dava erro
                 return
             
             if preco_minimo <= node.preco <= preco_maximo:
@@ -58,28 +63,80 @@ class ArvoreJogos:
         return jogos_achados
     
 
-arvore_jogos = ArvoreJogos()
-arvore_jogos.inserir(1, "Red Dead Redemption 2", "Rockstar Games", 250, "Aventura")
-arvore_jogos.inserir(2, "Call of Duty: Black Ops 2", "Activision", 350, "Tiro")
-arvore_jogos.inserir(3, "Gta V", "Rockstar Games", 200, "Ação")
-arvore_jogos.inserir(4, "BloodBorne", "From Software", 100, "RPG")
+class HashGeneros:
+    def __init__(self):
+        self.genero_para_jogos = {}
+
+    def adicionar_jogo(self, jogo):
+        for genero in jogo.genero: 
+            genero_hash = hash(genero) 
+            if genero_hash not in self.genero_para_jogos:
+                self.genero_para_jogos[genero_hash] = []
+            self.genero_para_jogos[genero_hash].append(jogo)
+
+    def buscar_por_genero(self, genero):
+        genero_hash = hash(genero) 
+        return self.genero_para_jogos.get(genero_hash, [])
+
+class MotorBuscaJogos:
+    def __init__(self):
+        self.arvore_jogos = ArvoreJogos()
+        self.generos = HashGeneros()
+
+    def inserir_jogo(self, jogo_id, titulo, desenvolvedor, preco, genero):
+        novo_jogo = NoJogo(jogo_id, titulo, desenvolvedor, preco, genero)
+        self.arvore_jogos.inserir(jogo_id, titulo, desenvolvedor, preco, genero)
+        self.generos.adicionar_jogo(novo_jogo)
+
+    def buscar_jogos_por_preco(self, preco):
+        return self.arvore_jogos.buscar_por_preco(preco)
+
+    def buscar_jogos_por_faixa_preco(self, preco_minimo, preco_maximo):
+        return self.arvore_jogos.busca_por_faixa_preco(preco_minimo, preco_maximo)
+    
+    def buscar_por_genero(self, genero):
+        return self.generos.buscar_por_genero(genero)
+    
+
+loja_jogos = MotorBuscaJogos()
+loja_jogos.inserir_jogo(1, "Red Dead Redemption 2", "Rockstar Games", 250, ["Aventura", "Ação"])
+loja_jogos.inserir_jogo(2, "Call of Duty: Black Ops 2", "Activision", 250, ["Tiro", "Ação"])
+loja_jogos.inserir_jogo(3, "Fifa", "EA games", 200, ["Esportes"])
+loja_jogos.inserir_jogo(4, "BloodBorne", "From Software", 100, ["RPG", "Aventura"])
+loja_jogos.inserir_jogo(5, "The Last of Us", "Naughty Dog", 180, ["Aventura", "Ação"])
+loja_jogos.inserir_jogo(6, "Fallout: New Vegas", "Obsidian Entertainment", 170, ["RPG"])
 
 
-procurar_jogo_preco = arvore_jogos.buscar_por_preco(250)
 
-if procurar_jogo_preco:
-    print(f"Jogo encontrado: {procurar_jogo_preco.titulo}")
-else:
-    print("Jogo não encontrado.")
+print("Jogos com um preço específico: ")
+procurar_jogo_preco = loja_jogos.buscar_jogos_por_preco(250)
+
+for jogo in procurar_jogo_preco:
+    print(f"jogo: {jogo.titulo} | preço: {jogo.preco}")
+
 
 print("######################")
 
-jogos_faixa_preco = arvore_jogos.busca_por_faixa_preco(100, 200)
+print("Jogos com uma faixa de preço: ")
+jogos_faixa_preco = loja_jogos.buscar_jogos_por_faixa_preco(100, 200)
 
-for jogo in jogos_faixa_preco:
-    print(f"Jogo: {jogo.titulo} | Preço: {jogo.preco}")
-
-    
-
+for jogo in jogos_faixa_preco: # como ali na linha 117 ele retona uma lista, aqui eu vou percorrer ela pra mostrar os jogos achados, a mesma coisa no buscar por preço e por gênero
+    print(f"jogo: {jogo.titulo} | preço: {jogo.preco}")
 
 
+print("######################")
+
+print("Jogos por gênero: ")
+jogos_genero = loja_jogos.buscar_por_genero("Aventura")
+
+for jogo in jogos_genero:
+    print(f"jogo: {jogo.titulo} | gênero: {jogo.genero}")
+
+
+print("######################")
+
+print("Jogos organizados por gênero (conteúdo do dicionário):")
+for genero_hash, jogos in loja_jogos.generos.genero_para_jogos.items():
+    print(f"Gênero Hash: {genero_hash}")
+    for jogo in jogos:
+        print(f"  - Jogo: {jogo.titulo} | Gêneros: {jogo.genero}")
